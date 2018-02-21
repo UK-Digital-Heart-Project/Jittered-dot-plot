b00c18186c48cf5a742a2148064368d54f900279
Declan O'Regan <declan.oregan@imperial.ac.uk>
2018-02-21 14:55:46 +0000

    Update Jittered box plot.R

--- Jittered box plot.R
+++ Jittered box plot.R
@@ -12,19 +12,20 @@ theme_update(axis.ticks.x = element_blank(), axis.line.y=element_line(colour="bl
              panel.grid.minor = element_blank(),  panel.background = element_blank(), axis.text.x  = element_text(size=20), axis.text.y  = element_text(size=20), axis.title.x  = element_text(size=22, vjust=0.1, face="bold"),
              axis.title.y  = element_text(size=22, face = "bold", vjust=0.9, angle = 90), axis.line = element_line(size = 1.2, linetype = "solid"), axis.ticks = element_line(size = 1), legend.position="none") 
 
-#Create dummy data, n=1000, IQ values, split into groups
+#Create dummy data
 
 test<-r_data_frame(n=1000,
                    iq,
-                   group(x=c("X", "Y", "C", "D"))
+                   group(x=c("X", "Y"))
 )
 
-#Flag some of the IQ values to show as different colours in dot plot later
+#Flag some of these to show in dot plot later
 
 test$Cat<-as.factor(ifelse(test$IQ>(sample(110:125,1000,replace=TRUE)),"Normal", "Abnormal"))
 
-#Transparent box plots (Groups X and Y) with jittered data points coloured as red or blue according to the "Cat" flag set above.
 
+#Dot plot
+#Two groups with transparent box plots and jittered data points coloured by category
 ggplot(na.omit(test[,c("Group", "IQ", "Cat")]), aes(factor(Group), IQ)) +
   geom_jitter(alpha=I(0.3), position=position_jitter(width=0.1, height=0.1), aes(color=factor(Cat)), size=4) +
   xlab("\nGroups") +
@@ -32,3 +33,4 @@ ggplot(na.omit(test[,c("Group", "IQ", "Cat")]), aes(factor(Group), IQ)) +
   geom_boxplot(outlier.shape=NA, fill= NA,  colour="black", width=0.15, lwd=1) + scale_colour_manual(values=c("lightskyblue1", "red")) +
   scale_y_continuous (limits=c(50,150), expand=c(0,0.2)) +
   ggtitle (" ")
+
