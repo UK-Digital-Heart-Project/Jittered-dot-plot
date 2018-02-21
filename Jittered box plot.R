14dcd26efb2c5f82b6032ae7653d1eecafbb74b9
Declan O'Regan <declan.oregan@imperial.ac.uk>
2018-02-21 14:59:24 +0000

    Update Jittered box plot.R

--- Jittered box plot.R
+++ Jittered box plot.R
@@ -19,7 +19,7 @@ test<-r_data_frame(n=1000,
 )
 
 #Flag some of the higher IQ values as a different colour in the dot plot.
-#Could be any pre-defined factor in the data you want to distinguish
+#In practice would be any pre-defined factor in the data you want to distinguish.
 
 test$Cat<-as.factor(ifelse(test$IQ>(sample(110:125,1000,replace=TRUE)),"Normal", "Abnormal"))
 
